9a7730cb3a7e7fa6f3892f0ff43c614ecefc6aa8
David Folks <aldwyn.wot@gmail.com>
2018-07-16 12:29:32 -0600

    created a seperate function for yearOfBirth

--- drills1.js
+++ drills1.js
@@ -1,7 +1,12 @@
 function whoAmI(name, age) {
-    let yearOfBirth = 2018-age;
+    let yob= yearOfBirth(age);
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in ${yearOfBirth}`);
+    console.log(`I was born in ${yob}`);
 }
 
+function yearOfBirth(age){
+    return 2018-age;
+ }
+
 whoAmI("john", 53);
+yearOfBirth(53);
