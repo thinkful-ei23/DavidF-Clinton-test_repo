0aadb7d4d5e7f38b22f4bed562adc3cd2b77e570
David Folks <aldwyn.wot@gmail.com>
2018-07-16 13:43:44 -0600

    added toInfinity and JediName

---
+++ drills2.js
@@ -0,0 +1,26 @@
+function jediName(firstName, lastName) {
+    return lastName.slice(0,3) + firstName.slice(0, 2);
+}
+
+console.log(jediName('Beyonce', 'Knowles'));
+
+function beyond(num) {
+   if (Math.abs(num) === Number.POSITIVE_INFINITY) {
+       console.log('And beyond');
+   } else if (num > 0) {
+        console.log('To infinity');
+   } else if (num < 0) {
+       console.log('To negative infinity');
+   } else if (num === 0) {
+       console.log('Staying home');
+   } else {
+       console.log('Please use a number!');
+   }
+}
+
+beyond(Number.POSITIVE_INFINITY);
+beyond(Number.NEGATIVE_INFINITY);
+beyond(23);
+beyond(-23);
+beyond(0);
+beyond('rudabega');
