1850af2a2bfae9e79f177da73c188121cc539a26
Clinton Owen <clint@clintonowen.com>
2018-07-16 15:01:04 -0500

    created decode function

--- drills2.js
+++ drills2.js
@@ -32,4 +32,34 @@ beyond('rudabega');
 */
 
 //Cracking the Code
+/*
+function decode(word) {
+    let firstChar = word[0];
+    if (firstChar === 'a') {
+        return word[1];
+    } else if (firstChar === 'b') {
+        return word[2];
+    } else if (firstChar === 'c') {
+        return word[3];
+    } else if (firstChar === 'd') {
+        return word[4];
+    } else {
+        return ' ';
+    }
+}
 
+let testArray = ['craft', 'block', 'argon', 'meter', 'bells', 'brown', 'croon', 'droop'];
+
+let decoded = '';
+
+for (let i = 0; i < testArray.length; i++) {
+    decoded += decode(testArray[i]);
+}
+
+console.log(decode('apple'));
+console.log(decode('banan'));
+console.log(decode('cranb'));
+console.log(decode('david'));
+console.log(decode('emmet'));
+console.log(decoded);
+*/
