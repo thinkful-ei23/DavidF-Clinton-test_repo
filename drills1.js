b892e3b6caf3751bd2384357cb2a782db4c8498b
David Folks <aldwyn.wot@gmail.com>
2018-07-16 12:19:57 -0600

    added yearOfBirth variable to whoAmI

--- drills1.js
+++ drills1.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age) {
+    let yearOfBirth = 2018-age;
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI("john", 53);
