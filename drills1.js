28394bf7f1c82f6200f278edb17640e7dca06d83
David Folks <aldwyn.wot@gmail.com>
2018-07-16 12:17:10 -0600

    added parameters to whoAmI

--- drills1.js
+++ drills1.js
@@ -1,5 +1,5 @@
-function whoAmI() {
-    console.log("Hi, my name is Chris and I'm 29 years old");
+function whoAmI(name, age) {
+    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 }
 
-whoAmI();
+whoAmI("john", 53);
