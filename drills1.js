4986d62996049cdc0365456a9e6b15d7fa734d03
Clinton Owen <clint@clintonowen.com>
2018-07-16 14:18:47 -0500

    added try/catch and conditionals

--- drills1.js
+++ drills1.js
@@ -1,12 +1,37 @@
+'use strict';
+
 function whoAmI(name, age) {
     let yob= yearOfBirth(age);
-    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in ${yob}`);
+    try {
+        if (!name || !age) {
+            throw new Error('Arguments not valid');
+        } else {
+            console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+            console.log(`I was born in ${yob}`);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+    // if (yob === !NaN) {
+    //     console.log(`I was born in ${yob}`);
+    // } else {
+    //     console.log('You haven\'t been born');
+    // }
 }
 
 function yearOfBirth(age){
-    return 2018-age;
+    let yob = 2018-age;
+    try {
+        if (yob > 2018) {
+            throw new Error('Age can not be negative');
+        } else {
+            return yob;
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
  }
 
-whoAmI("john", 53);
+whoAmI('john', -2);
 yearOfBirth(53);
